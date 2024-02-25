29da2dcb3e952e2c3ca6493dc8ce87875ac82406
amirth300324@gmail.com <amirth300324@gmail.com>
2024-02-25 14:19:28 +0530

    Added Play Page

---
+++ src/routes/play/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import TextInput from "$lib/components/TextInput.svelte";
+    import SubmitButton from "$lib/components/SubmitButtom.svelte";
+</script>
+
+<main class="absolute top-0 left-0 -z-10 h-screen w-screen py-16 px-4 flex justify-center items-center bg-gray-700">
+
+    <section class="w-full shadow-md shadow-gray-800 items-center flex flex-col gap-4 bg-gray-600 p-4 rounded-md text-gray-200">
+        <h1 class="text-xl">Solve the Expression</h1>
+        <p>5 + 8  - 9</p>
+        <TextInput label="Answer" placeholder="Answer" name="answer"/>
+        <SubmitButton />
+    </section>
+
+</main>
